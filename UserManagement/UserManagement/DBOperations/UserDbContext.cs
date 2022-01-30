a0097c173fe255e724018531c363c230098631da
aktasertan11 <aktasertan11@gmail.com>
2022-01-30 03:55:55 +0300

    intial Commit

---
+++ UserManagement/UserManagement/DBOperations/UserDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserManagement.DBOperations
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
